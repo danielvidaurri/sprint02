15db4aaa07737a106b3e45590de2c57c0b24c0b9
danielvidaurri <si709570@iteso.mx>
2021-11-07 23:12:18 -0600

    .

--- app.js
+++ app.js
@@ -9,5 +9,17 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
+app.get('/html/signup.html', function(req,res) {
+    res.sendFile(path.join(__dirname + '/html/signup.html'));
+});
+
+app.get('/html/login.html', function(req,res) {
+    res.sendFile(path.join(__dirname + '/html/login.html'));
+});
+
+app.get('/html/dashboard.html', function(req,res) {
+    res.sendFile(path.join(__dirname + '/html/dashboard.html'));
+});
+
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
