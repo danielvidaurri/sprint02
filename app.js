5c9fe5628613a07154a6f840c92e2ec8b8449234
danielvidaurri <si709570@iteso.mx>
2021-11-07 22:31:51 -0600

    .

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
-// importamos las librerias importantes
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config();
+const http = require('http');
 
-// importamos el router
-const router = require('index.html');
+const hostname = '127.0.0.1';
+const port = 3000;
 
-// de express nos traemos lo necesario
-const { json, urlencoded } = express
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hola Mundo');
+});
 
-// creamos nuestra app
-const app = express()
-
-// definimos un puerto por el cual escucharemos peticiones
-const PORT = process.env.PORT || 8080
-const HOST = process.env.HOST || "0.0.0.0"
-
-// configuraciones para nuestro server
-app.use(json())
-app.use(urlencoded({ extended: false }))
-const corsOptions = { origin: '*', optionsSuccessStatus: 200 }
-app.use(cors(corsOptions))
-
-// indicamos que usaremos un router
-app.use(router)
-
-// iniciamos nuestro server
-app.listen(PORT,HOST, () => { console.log(`Server listening on port ${PORT} and host ${HOST}`); })
+server.listen(port, hostname, () => {
+  console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+});
